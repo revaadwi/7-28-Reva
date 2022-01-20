04d872e2169d7dfb8ba5c3fa407b155d42ed3513
unknown <xrevadf@gmail.com>
2022-01-20 15:48:00 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Halaman Home';
+    return view ("Home");
 });
 
 Route::get('/about', function () {
-    return 'Halaman About';
+    return view ("about");
 });
 
 Route::get('gallery', function () {
-    return 'Halaman Gallery';
+    return view ("gallery");
 });
